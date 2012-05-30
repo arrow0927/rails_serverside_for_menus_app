6e0b2939e69a33018686816209bc51babe5d548f
Ash <amulekar@gmail.com>
2012-05-30 08:01:22 -0700

    Safari Patch for security hole

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,10 @@ class ApplicationController < ActionController::Base
   # Its important to manage a session so that when a user logs out the cookies are not stored
   #beyond the session
   def set_no_cache
-    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
     response.headers["Pragma"] = "no-cache"
-    #response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
-    response.headers["Expires"] = "#{1.year.ago}"
+    response.headers["Cache-Control"] = "no-cache, private, max-stale=0, post-check=0, pre-check=0, no-store, max-age=0, must-revalidate"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+    response.headers["Vary"] = "*"
   end
   
 end
